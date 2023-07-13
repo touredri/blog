0b10353f08629d7995ccce29a8ba15109a0950cc
touredri <dcompte223@gmail.com>
2023-07-13 10:15:37 +0000

    added tables models

---
+++ db/migrate/20230713095930_create_likes.rb
@@ -0,0 +1,10 @@
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.references :user, null: false, foreign_key: true, on_delete: :cascade
+      t.references :post, null: false, foreign_key: true, on_delete: :cascade
+
+      t.timestamps
+    end
+  end
+end
