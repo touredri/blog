a26104b6787abe6f011a685f99148b7233685d54
touredri <dcompte223@gmail.com>
2023-07-15 10:53:21 +0000

    change user_id to author_id for likes & comments tables

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   after_save :update_like_counter
