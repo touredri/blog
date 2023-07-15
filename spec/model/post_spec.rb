e9cac5ec9d3ae5ece38c04b9996905918a237e1f
touredri <dcompte223@gmail.com>
2023-07-15 08:57:22 +0100

    validation done

---
+++ spec/model/post_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe 'Post' do
+  subject { Post.new }
+  before { }
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
+  end
+end
