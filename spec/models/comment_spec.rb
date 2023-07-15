8074d9ae802ea95e2748289095f87a5424ec5bf1
touredri <dcompte223@gmail.com>
2023-07-15 12:47:30 +0000

    added test

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Comment, type: :model do
+  @user = User.new(name: 'Sam', photo: 'photo.url', bio: 'Excellent')
+  @post = Post.new(author: @user, title: 'Let us go', text: 'This is the text for the post')
+
+  subject { Comment.create(post: @post, author: @user, text: 'My comment is here') }
+
+  before { subject.save }
+
+  it 'should have an author' do
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a post' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should update the comments_counter' do
+    new_user = User.create(name: 'John', photo: 'photo_url', bio: 'Professor', posts_counter: 0)
+    new_post = Post.create(author: new_user, title: 'Choose', text: 'text for the post',
+                           comments_counter: 0, likes_counter: 0)
+    Comment.create(post: new_post, author: new_user, text: 'New comment text')
+    expect(new_post.comments_counter).to eq(1)
+  end
+end
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Like, type: :model do
+  @user = User.new(name: 'Sam', photo: 'photo.url', bio: 'Excellent')
+  @post = Post.new(author: @user, title: 'Let us go', text: 'This is the text for the post')
+
+  subject { Like.create(post: @post, author: @user) }
+
+  before { subject.save }
+
+  it 'should have an author' do
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a post' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should update the comments_counter' do
+    new_user = User.create(name: 'John', photo: 'photo_url', bio: 'Professor', posts_counter: 0)
+    new_post = Post.create(author: new_user, title: 'Choose', text: 'text for the post',
+                           comments_counter: 0, likes_counter: 0)
+    Like.create(post: new_post, author: new_user)
+    expect(new_post.likes_counter).to eq(1)
+  end
+end
