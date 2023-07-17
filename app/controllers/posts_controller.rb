c3febe97bbfd3494b74c64f77dae3f32ceff60cb
touredri <dcompte223@gmail.com>
2023-07-17 23:45:25 +0100

    added controllers & routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    render json: Post.find(params[:id])
+  end
+end
