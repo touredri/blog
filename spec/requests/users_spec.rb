9d0feb86c086cb4f5c0c78eeeaaba0dfd555109c
touredri <dcompte223@gmail.com>
2023-07-20 23:45:49 +0100

    added test for user show action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,8 +19,28 @@ end
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'should show the placeholder' do
-      get '/users/index'
+      get '/users'
+      expect(response.body).to include('Hello world')
+    end
+  end
+  describe 'Get /show' do
+    it 'should show the placeholder' do
+      get '/users/1'
       expect(response.body).to include('This is the list of a user with id')
+    end
+  end
+end
+
+RSpec.describe 'Show', type: :request do
+  describe 'GET /show' do
+    it 'returns a successful response' do
+      get '/users/show'
+      expect(response).to be_successful
+    end
+
+    it 'renders the show template' do
+      get '/users/show'
+      expect(response).to render_template('show')
     end
   end
 end
