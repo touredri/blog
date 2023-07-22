f0d2fb37241845b3290c96303641f3d459bb8cfd
touredri <dcompte223@gmail.com>
2023-07-22 13:24:13 +0100

    added form for database insersion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def current_user
+    # will be available in all controllers
+    @current_user = User.find(1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
+  end
+  resources :posts, only: %i[new create] do
+    resources :comments, only: %i[new create]
   end
 end
