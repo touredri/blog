94cb0af86c706b79b5b422ef0f9a1468c0420b6f
touredri <dcompte223@gmail.com>
2023-07-22 01:20:50 +0100

    view completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id:params[:id])
+    @user = User.find_by(id: params[:id])
     @recent_posts = @user.user_recent_posts
   end
 end
--- db/migrate/20230721063233_add_default_posts_counter_to_users.rb
+++ db/migrate/20230721063233_add_default_posts_counter_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDefaultPostsCounterToUsers < ActiveRecord::Migration[7.0]
   def change
     change_column_default :users, :posts_counter, 0
