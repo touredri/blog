67db6dd080ab43f3d6330634cafd830a0c0edb2f
Mercy Mugambi <48310207+mercymugambi@users.noreply.github.com>
2023-07-27 20:05:50 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
-# frozen_string_literal: true
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
+end
